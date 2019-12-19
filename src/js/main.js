ee3c0ef766b3d381a9ed8f30509d1cb14fc1432d
llflannery <lflannery@seattletimes.com>
2019-12-18 17:13:39 -0800

    changed break point to mobile not tablet

--- src/js/main.js
+++ src/js/main.js
@@ -15,29 +15,34 @@ require("component-responsive-frame/child");
 // label:nth-of-type(1)
 
 $( ".toggle_radio label" ).click(function() {
-  console.log( $(this).attr('id') );
+
   if ( $(this).attr('id') === "beforeLabel" ) {
-    $('.beforeSummary').fadeIn();
-    $('.afterSummary').hide();
-    $(".toggle_radio").toggleClass("toggleOn");
-    $(".divider").toggleClass("toggleOn");
+    $('.before').fadeIn();
+    $('.after').hide();
+
     $(this).find('div').addClass("opacityOn");
     $('#afterLabel').find('div').removeClass('opacityOn');
-    $('.beforeIntro').show();
-    $('.afterIntro').hide();
 
-    $(".stats").css("border-left","10px solid #4bb3d6");
+
+    $(".toggle_radio").removeClass("toggleOn");
+    $(".divider").removeClass("toggleOn");
+
+
+    ($( window ).width() > 480) ? $(".stats").css("border-left","10px solid #4bb3d6") : '' ;
+
+
   } else {
-    $('.afterSummary').fadeIn();
-    $('.beforeSummary').hide();
+    $('.before').hide();
+    $('.after').fadeIn();
 
-    $(".toggle_radio").toggleClass("toggleOn");
-    $(".divider").toggleClass("toggleOn");
+    $(".toggle_radio").addClass("toggleOn");
+    $(".divider").addClass("toggleOn");
 
     $(this).find('div').addClass("opacityOn");
     $('#beforeLabel').find('div').removeClass('opacityOn');
-    $('.beforeIntro').hide();
-    $('.afterIntro').show();
-    $(".stats").css("border-left","10px solid #0e5773");
+
+    ($( window ).width() > 480) ? $(".stats").css("border-left","10px solid #0e5773") : '' ;
+
+
   }
 });
